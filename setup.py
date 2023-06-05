d03a3b6800a10d01afc7d0e2286f36c4da64628e
NXTDMNSN <98911556+NXTDMNSN@users.noreply.github.com>
2023-06-04 18:59:12 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="KICK-GNR8R",
+  version="0.0.1",
+  description="",
+  packages=find_packages(),
+  install_requires=[
+    "torch",
+    "tqdm",
+  ]
+)
